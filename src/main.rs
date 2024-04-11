9cf29426e108ce168910903296d3779a582d0553
BigFish2086 <a8686.ibrahim@gmail.com>
2024-04-11 23:03:08 +0200

    6th stage, handle concurrent requests

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@ mod response;
 mod status;
 
 use std::io::{Read, Write};
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
+use std::thread;
 
 use crate::request::Request;
 use crate::response::Response;
@@ -32,47 +33,48 @@ fn echo_header(req: &Request, header: &str) -> Response {
     }
 }
 
+fn handle_stream(mut stream: TcpStream) {
+    println!("INFO: Incoming Connection {:?}", stream);
+    let mut buffer = [0u8; 1024];
+    match stream.read(&mut buffer) {
+        Ok(buffer_len) => {
+            let req = match Request::from_utf8(&buffer[..buffer_len]) {
+                Ok(req) => req,
+                Err(err) => {
+                    eprintln!("ERROR: {}", err);
+                    return;
+                }
+            };
+            if let Err(err) = match req.path {
+                ref path if path == "/" => {
+                    stream.write_all(&Response::new(StatusCode::OK, None, None).as_bytes())
+                }
+                ref path if path.starts_with("/echo/") => {
+                    stream.write_all(&echo_msg(&req).as_bytes())
+                }
+                ref path if path.starts_with("/user-agent") => {
+                    stream.write_all(&echo_header(&req, "User-Agent").as_bytes())
+                }
+                _ => stream.write_all(&Response::new(StatusCode::NOT_FOUND, None, None).as_bytes()),
+            } {
+                eprintln!("ERROR: {}", err);
+            }
+        }
+        Err(err) => {
+            eprintln!("ERROR: {}", err);
+        }
+    };
+}
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
-                println!("INFO: accepted new connection");
-                let mut buffer = [0u8; 1024];
-                match stream.read(&mut buffer) {
-                    Ok(buffer_len) => {
-                        let req = match Request::from_utf8(&buffer[..buffer_len]) {
-                            Ok(req) => req,
-                            Err(err) => {
-                                eprintln!("ERROR: {}", err);
-                                continue
-                            }
-                        };
-                        if let Err(err) = match req.path {
-                            path if path == "/" => stream
-                                .write_all(&Response::new(StatusCode::OK, None, None).as_bytes()),
-                            ref path if path.starts_with("/echo/") => {
-                                stream.write_all(&echo_msg(&req).as_bytes())
-                            }
-                            ref path if path.starts_with("/user-agent") => {
-                                stream.write_all(&echo_header(&req, "User-Agent").as_bytes())
-                            }
-                            _ => stream.write_all(
-                                &Response::new(StatusCode::NOT_FOUND, None, None).as_bytes(),
-                            ),
-                        } {
-                            eprintln!("ERROR: {}", err);
-                        }
-                    }
-                    Err(err) => {
-                        eprintln!("ERROR: {}", err);
-                    }
-                };
+            Ok(stream) => {
+                thread::spawn(move || handle_stream(stream));
             }
-            Err(err) => {
-                eprintln!("ERROR: {}", err);
-            }
+            Err(err) => eprintln!("ERROR: {}", err),
         }
     }
 }
