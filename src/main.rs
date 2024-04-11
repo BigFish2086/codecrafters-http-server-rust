87914d66732c1b00107257ba04057288c059aeeb
BigFish2086 <a8686.ibrahim@gmail.com>
2024-04-11 15:14:03 +0200

    2nd stage, respond with 200 ok

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,26 @@
 use std::net::TcpListener;
+use std::io::{Read, Write};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut stream) => {
+                println!("INFO: accepted new connection");
+                let mut buffer = [0u8; 1024];
+                match stream.read(&mut buffer) {
+                    Ok(buffer_len) => {
+                        println!("INFO: Read {} bytes\n{}", buffer_len, std::str::from_utf8(&buffer[..buffer_len]).unwrap());
+                        if let Err(err) = stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n") {
+                            print!("Error: {}", err);
+                        }
+                    }
+                    Err(err) => { println!("ERROR: {}", err); }
+                };
             }
-            Err(e) => {
-                println!("error: {}", e);
+            Err(err) => {
+                println!("ERROR: {}", err);
             }
         }
     }
